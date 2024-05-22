667d64e36ff2236cbda7e49b00014ad1693119f0
Syed Ashhad <syedashad17@gmail.com>
2024-05-23 00:20:20 +0500

    Final Push

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import Header from "./Components/Header";
 import Hero from "./Components/Hero"
+import Benefits from "./Components/Benefits"
+import Collaboration from "./Components/Collaboration"
+import Services from "./Components/Services"
+import Roadmap from "./Components/Roadmap"
+import Footer from "./Components/Footer"
+
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Pricing from "./Components/Pricing";
 
 const App = () => {
   return (
@@ -8,6 +15,12 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
         <Header />
         <Hero />
+        <Benefits/>
+        <Collaboration/>
+        <Services/>
+        <Pricing/>
+        <Roadmap />
+        <Footer/>
       </div>
       <ButtonGradient />
     </>
